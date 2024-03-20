12e922ffb4fafd119c008dad9cc7b327592ca800
achi113s <latourgiorgio@gmail.com>
2024-03-20 14:40:25 -0400

    add recipe prompt

--- Grocemate/Models/Constants.swift
+++ Grocemate/Models/Constants.swift
@@ -1,5 +1,5 @@
 //
-//  K.swift
+//  Constants.swift
 //  Grocemate
 //
 //  Created by Giorgio Latour on 12/18/23.
@@ -16,5 +16,17 @@ class Constants {
     components and output as a JSON object. The JSON object should \
     have the following format: {"ingredients": []}. \
     Here are the ingredients:
+    """
+
+    static let recipePrompt = """
+    Partition this food recipe into a JSON object which includes \
+    properties for the title (as a string), the individual recipe \
+    steps (as an array of strings), and the ingredients (as array \
+    of strings). There may be some portion of the recipe that includes \
+    a narrative description of the recipe, food pairing suggestions, or \
+    other text not directly related to the recipe instructions. \
+    You can put all of that into the notes field (as a string). The JSON \
+    object should have the following format: {"title": "", "steps": [], \
+    "ingredients": [], "notes": ""}. Here is the recipe:
     """
 }
